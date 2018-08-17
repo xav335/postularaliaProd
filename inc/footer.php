26b1c9b49e83fe71df02e2b986764f0e352e86ec
J. Gonzalez <fjavi.gonzalez@gmail.com>
2018-08-17 17:27:12 +0200

    footer en plus

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 			<footer>
 				<div class="row">
-					<div class="large-6 medium-6 small-12 columns">
+					<div class="large-4 medium-4 small-12 columns">
 						<quote>
 							« Superbe ambiance,<br/>
 							très bon feeling avec l’équipe.<br/>
@@ -8,8 +8,8 @@
 							<span>Jessica R.</span>
 						</quote>
 					</div>
-					<div class="large-6 medium-6 small-12 columns">
-						<p>postur<span>alia</span></p>
+					<div class="large-4 medium-4 small-12 columns">
+						<p>postur<span>alia</span> PAU</p>
 						<p>
 							Rue Lavoisier<br/>
 							64000 Pau
@@ -23,6 +23,22 @@
                                 mardi/jeudi 9h30/13h et 15h/20h<br>
                                 vendredi 9h30/13h et 15h/19h<br>
                                 samedi 9h30/13h
+							</p>
+					</div>
+					<div class="large-4 medium-4 small-12 columns">
+						<p>postur<span>alia</span> BORDEAUX</p>
+						<p>
+							72 avenue Thiers<br/>
+							33100 Bordeaux Bastide
+						</p>
+						<p>Tél. 05 56 32 62 10</p>
+						<p>
+							    <br>
+                                Accueil, inscription, coaching: <br>
+								Lundi-Mardi-Mercredi :<br> 9h30-13h30 / 15h-20h<br>
+                                Jeudi : 9h30-13h30 / 16h-20h<br>
+                                Vendredi : 9h30-12h30 / 16h-20h<br>
+                                Samedi : 9h30-12h30
 							</p>
 					</div>
 				</div>
