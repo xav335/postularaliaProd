7fa2a1d3c69d1765e1104baa75f0bc8ec61a26e4
J. Gonzalez <fjavi.gonzalez@gmail.com>
2019-04-01 19:52:52 +0200

    code google mail pro

--- inc/meta.php
+++ inc/meta.php
@@ -1,3 +1,4 @@
+		<meta name="google-site-verification" content="TjgYxapR2L0RdT1rasmTTeVdhtGO-jBRYBvaLTCYpjM" />
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
